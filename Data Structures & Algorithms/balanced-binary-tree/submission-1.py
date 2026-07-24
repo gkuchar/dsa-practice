d690aba893d0d10db39654649017bbf771ee07a4
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-07-24 18:11:06 -0500

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root: return True
+        is_balanced = True
+
+        def height(node) -> int:
+            nonlocal is_balanced
+
+            if node.left:
+                left = height(node.left) + 1
+            else:
+                left = 0
+            
+            if node.right:
+                right = height(node.right) + 1
+            else:
+                right = 0
+            
+            print(f'{(left - right)}')
+            if abs(left - right) > 1:
+                is_balanced = False
+
+            return max(left, right)
+        
+        height(root)    
+        return is_balanced
+        
