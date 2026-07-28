791f07d12ed3f49e1e322099a3a95395bbeda1fb
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-07-27 19:35:44 -0500

    Add: subtree-of-a-binary-tree - submission-5

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-5.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root: return False
+        if not subRoot: return True
+
+        def same_tree(root, sub) -> bool:
+            if not root and not sub: return True
+
+            stack_root = [root]
+            stack_sub = [sub]
+
+            while stack_root and stack_sub:
+                curr_root = stack_root.pop()
+                curr_sub = stack_sub.pop()
+
+                if not curr_root and not curr_sub:
+                    continue
+                
+                if (curr_root and not curr_sub) or (not curr_root and curr_sub) or curr_root.val != curr_sub.val:
+                    return False
+                
+                stack_root.append(curr_root.right)
+                stack_sub.append(curr_sub.right)
+
+                stack_root.append(curr_root.left)
+                stack_sub.append(curr_sub.left)
+            
+            return True
+        
+        if same_tree(root, subRoot):
+            return True
+        else:
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+        # T = O(n * m), n = # nodes in root, m = # nodes in subRoot
+        # S = O(n + m)
