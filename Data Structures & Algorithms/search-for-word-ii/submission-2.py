b009795e8b79032f579c8e8547f4ad7eb4eee9cf
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-08-05 08:52:04 -0500

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.py
@@ -0,0 +1,60 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        # build trie from words
+        # dfs on each cell in grid, traversing trie in lockstep
+        # only continue dfs if next char is in trie curr_layer
+        # if reach end of word (marked by word field), append word
+
+        def build_trie(words):
+            main = {}
+            for word in words:
+                curr_layer = main
+                for char in word:
+                    if char not in curr_layer:
+                        curr_layer[char] = {}
+                    curr_layer = curr_layer[char]
+                curr_layer['word'] = word
+            
+            return main
+        
+        main = build_trie(words)
+        rows = len(board)
+        cols = len(board[0])
+        result = []
+        
+        def dfs(row, col, curr_layer, visited):
+            nonlocal result
+
+            if not (row < rows and row >= 0 and col < cols and col >= 0):
+                return
+            
+            if (row, col) in visited:
+                return
+            
+            curr_char = board[row][col]
+            if curr_char not in curr_layer:
+                return
+            
+            if 'word' in curr_layer[curr_char]:
+                result.append(curr_layer[curr_char]['word'])
+                curr_layer[curr_char].pop('word')
+            
+            visited.add((row, col))
+            
+            directions = [(1,0), (-1,0), (0, 1), (0, -1)]
+
+            for x, y in directions:
+                dfs(row + x, col + y, curr_layer[curr_char], visited)
+            visited.remove((row, col)) 
+        
+        for i in range(rows):
+            for j in range(cols):
+                dfs(i, j, main, set())
+        
+        return result
+
+        # T = O(rows * cols # 3^L), L = length of longest words
+        # 3^L since each dfs call moves in 3 with each direction going L more cells at the most
+
+        # S = O(s), s = sum of all chars in words.
+        # s dominates recursive call stack and visted set
