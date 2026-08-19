1b523c882bd52c294a6b0715561714ac22481c6d
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-08-19 08:50:23 -0500

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,49 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        empty_board = [
+            ["." for _ in range(n) ] for _ in range(n)
+        ]
+
+        result = []
+
+        def is_valid_queen(board, row, i):
+
+            for r in range(row):
+                if board[r][i] == 'Q':
+                    return False
+            
+            curr_i = i
+            curr_row = row
+            while curr_i < len(board) and curr_row > -1:
+                if board[curr_row][curr_i] == 'Q':
+                    return False
+                curr_i += 1
+                curr_row -= 1
+            
+            curr_i = i
+            curr_row = row
+            while curr_i > -1 and curr_row > -1:
+                if board[curr_row][curr_i] == 'Q':
+                    return False
+                curr_i -= 1
+                curr_row -= 1
+            
+            return True
+            
+
+        def dfs_bt(board, row):
+            nonlocal result
+
+            if row == len(board):
+                result.append([''.join(row) for row in board])
+                return
+            
+            for i in range(len(board)):
+                if is_valid_queen(board, row, i):
+                    board[row][i] = 'Q'
+                    dfs_bt(board, row + 1)
+                    board[row][i] = '.'
+            
+
+        dfs_bt(empty_board, 0)
+        return result
