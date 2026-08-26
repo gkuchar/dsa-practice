a6efcb797760718aa0504275ca0a50b76960603b
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-08-26 09:39:09 -0500

    Add: max-area-of-island - submission-1

---
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        largest = 0
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def search(i: int, j: int) -> int:
+            size = 1
+            grid[i][j] = 2
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+            def are_valid_indicies(x, y):
+                return x > -1 and x < rows and y > -1 and y < cols
+            
+            for dx, dy in directions:
+                x = i + dx
+                y = j + dy
+
+                if are_valid_indicies(x, y) and grid[x][y] == 1:
+                    size += search(x, y)
+            
+            return size
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    size = search(i, j)
+                    largest = max(largest, size)
+        
+        return largest
+        # T = O(n * m), n = # rows, m = # cols
+        # S = O(l), l = size of largest island: space allocated on call stack from recursion
