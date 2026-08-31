7dbab6e983e21c2a3242f498e18f89a80281c9ea
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-08-31 12:19:09 -0500

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = {i : [] for i in range(numCourses)}
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+        
+        valid_nodes = set()
+
+        def dfs_cycle_detect(node, visited):
+            if node in valid_nodes:
+                return True
+
+            for adj in graph[node]:
+                if adj in visited:
+                    return False
+                
+                visited.add(adj)
+                is_valid = dfs_cycle_detect(adj, visited)
+                if not is_valid:
+                    return False
+                else:
+                    visited.remove(adj)
+                    valid_nodes.add(adj)
+            
+            return True
+
+        for node in graph:
+            if not dfs_cycle_detect(node, visited=set([node])):
+                return False
+
+            valid_nodes.add(node)
+        
+        return True
+        # T = O(V + E)
+        # S = O(V)
