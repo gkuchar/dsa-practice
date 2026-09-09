c119658777477a8f6448f88dc4e0594cc55f0a5e
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-09-09 11:42:54 -0500

    Add: reconstruct-flight-path - submission-1

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = {}
+        out = {}
+        ordering = []
+        edge_count = {}
+
+        for edge in tickets:
+            u = edge[0]
+            v = edge[1]
+
+            if u not in graph:
+                graph[u] = []
+                out[u] = 0
+            
+            if v not in out:
+                graph[v] = []
+                out[v] = 0
+
+            if (u, v) not in edge_count:
+                edge_count[(u, v)] = 0
+            
+            graph[u].append(v)
+            out[u] += 1
+            edge_count[(u, v)] += 1
+             
+        def euler_path(node):
+            for adj in sorted(graph[node]):
+                if out[node] != 0 and edge_count[(node, adj)] != 0:
+                    out[node] -= 1
+                    edge_count[(node, adj)] -= 1
+                    euler_path(adj)
+            
+            ordering.append(node)
+                    
+        
+        euler_path('JFK')
+
+        return ordering[::-1]
+        
