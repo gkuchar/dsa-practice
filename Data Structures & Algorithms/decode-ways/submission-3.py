fb29433852db75d73175fae5d305197bceff3d3d
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-09-25 12:09:49 -0500

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = {}
+
+        def count_decoding(i):
+            if i in dp: return dp[i]
+
+            if i >= n:
+                dp[i] = 1
+                return 1
+            
+            count = 0
+            if s[i] != '0':
+                count += count_decoding(i + 1)
+            if i + 1 < n and s[i] != '0' and int(s[i:i+2]) < 27:
+                count += count_decoding(i + 2)
+            
+            dp[i] = count
+            return count
+
+        
+        return count_decoding(0)
+        # T = O(n)
+        # S = O(n), space allocated on recursive call stack
+            
+
+            
+
+        
